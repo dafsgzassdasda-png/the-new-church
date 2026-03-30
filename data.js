ec99b6a172ad43523230b66ece6e24a53af526aa
dafsgzassdasda-png <dafsgzassdasda@gmail.com>
2026-03-30 17:51:00 -0300

    Add files via upload

---
+++ data.js
@@ -0,0 +1,287 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-banheiro-02",
+      "name": "banheiro 02",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.9398482109022446,
+          "pitch": 0.29359670203442434,
+          "rotation": 0,
+          "target": "4-parte-de-tras-da-igreja"
+        },
+        {
+          "yaw": -0.31662211235109616,
+          "pitch": 0.24548608141706474,
+          "rotation": 0.7853981633974483,
+          "target": "2-centro-da-igreja"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-banhero-01",
+      "name": "banhero 01",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.0568426422135317,
+          "pitch": 0.9756312999660892,
+          "rotation": 0,
+          "target": "4-parte-de-tras-da-igreja"
+        },
+        {
+          "yaw": -0.011300856702211703,
+          "pitch": 0.8328002285683205,
+          "rotation": 0.7853981633974483,
+          "target": "2-centro-da-igreja"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-centro-da-igreja",
+      "name": "centro da igreja",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.5639375199844938,
+          "pitch": 0.4864333608265383,
+          "rotation": 0,
+          "target": "4-parte-de-tras-da-igreja"
+        },
+        {
+          "yaw": -2.1694449574653234,
+          "pitch": 0.5896389400778599,
+          "rotation": 4.71238898038469,
+          "target": "3-dentro-do-pergolado"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-dentro-do-pergolado",
+      "name": "dentro do pergolado",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.34160497120025113,
+          "pitch": 0.5322732811128148,
+          "rotation": 0,
+          "target": "2-centro-da-igreja"
+        },
+        {
+          "yaw": -0.9903924672926507,
+          "pitch": 0.5453879988331245,
+          "rotation": 0,
+          "target": "4-parte-de-tras-da-igreja"
+        },
+        {
+          "yaw": 2.885365148938642,
+          "pitch": 0.8619078733826058,
+          "rotation": 0,
+          "target": "5-pergolado-longe"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-parte-de-tras-da-igreja",
+      "name": "parte de tras da igreja",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.9628665114691977,
+          "pitch": 0.42202459755800703,
+          "rotation": 0,
+          "target": "1-banhero-01"
+        },
+        {
+          "yaw": 0.06214730217200781,
+          "pitch": 0.2685223395512075,
+          "rotation": 0,
+          "target": "0-banheiro-02"
+        },
+        {
+          "yaw": -1.6842744055548522,
+          "pitch": 0.5075805663494517,
+          "rotation": 0,
+          "target": "2-centro-da-igreja"
+        },
+        {
+          "yaw": -0.6677704068243848,
+          "pitch": 0.4934789535945825,
+          "rotation": 0,
+          "target": "3-dentro-do-pergolado"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-pergolado-longe",
+      "name": "pergolado longe",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.6509508754238933,
+          "pitch": 0.703265600204908,
+          "rotation": 0,
+          "target": "3-dentro-do-pergolado"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "crurch",
+  "settings": {
+    "mouseViewMode": "qtvr",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
